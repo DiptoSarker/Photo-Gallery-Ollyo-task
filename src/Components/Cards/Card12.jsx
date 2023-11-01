281586f6b26860ec91f45bbdff9a6f5e5157fd67
Dipto Sarker <diptosarker802@gmail.com>
2023-11-01 21:10:55 +0600

    df

--- src/Components/Cards/Card12.jsx
+++ src/Components/Cards/Card12.jsx
@@ -7,7 +7,7 @@ const Card12 = () => {
     <div class="upload-image-container">
       <label for="image-upload" class="upload-label">
         <GrGallery class="upload-logo" />
-        Add Images
+        Add Image
       </label>
 
       <input
